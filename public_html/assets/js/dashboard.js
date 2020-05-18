c3f10ded122987ec06144604b874e6e2c1acbe85
Travassos <felipe.a.travassos@accenture.com>
2020-05-18 20:49:49 -0300

    testjs

--- public_html/assets/js/dashboard.js
+++ public_html/assets/js/dashboard.js
@@ -3,16 +3,7 @@ $(function () {
 
   
 
-        $('.mobile').mask('00 0 0000.0000');
-        $('.mobile').mask('00 0 0000.0000');
-        $('.money').mask("#.##0,00", {reverse: true});
-        $('.onlynumbers').mask('000000000000000000000000000000');
-        $('.date').mask('00/00/0000');
-        $('.summernote').summernote();
-        $('.addresssummernote').summernote({
-            height: 100,
-            toolbar: false
-        });
+      
 		
 		
 		alert(1);
